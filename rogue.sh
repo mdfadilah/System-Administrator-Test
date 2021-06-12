b448e57c16a75b646bfa4da678ce936d6e2ae369
mdfadilah <85622881+mdfadilah@users.noreply.github.com>
2021-06-12 23:04:01 +0800

    Update rogue.sh

--- rogue.sh
+++ rogue.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sysname=`uname -n`
-filename='roguefile'
-find / -type f -size +1G -printf '%s %p\n' > $filename
+filenames='roguefile'
+find / -type f -size +1G -printf '%s %p\n' > $filenames
 count=`cat roguefile|wc -l`
 if [$? -ne 0]
 then
-  date>>$filename
-  mail -s "Attention large rogue file found on $sysname" alarm@animapoint.net < $filename
+  date>>$filenames
+  mail -s "Attention large rogue file found on $sysname" alarm@animapoint.net < $filenames
 fi
 	
